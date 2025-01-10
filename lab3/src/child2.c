6402c3e3f9507e8d95e722e9cc7be235e91aa1d0
Ivan <mrvanivan@mail.ru>
2025-01-10 12:08:34 +0300

    minor fixes

--- lab3/src/child2.c
+++ lab3/src/child2.c
@@ -13,20 +13,22 @@
 #define BUF_SIZE 4096
 
 int main() {
+
     int shm_fd = shm_open(SHM_NAME, O_RDWR, 0666);
     if (shm_fd == -1) {
         perror("Ошибка открытия памяти в child2");
         exit(EXIT_FAILURE);
     }
+    char *shared_memory = mmap(NULL, BUF_SIZE, PROT_READ | PROT_WRITE, MAP_SHARED,
+                               shm_fd, 0);
 
-    char *shared_memory = mmap(NULL, BUF_SIZE, PROT_READ | PROT_WRITE, MAP_SHARED, shm_fd, 0);
     if (shared_memory == MAP_FAILED) {
         perror("Ошибка маппинга памяти в child2");
         exit(EXIT_FAILURE);
     }
-
     sem_t *sem_read2 = sem_open(SEM_READ2_NAME, 0);
     sem_t *sem_write2 = sem_open(SEM_WRITE2_NAME, 0);
+
     if (!sem_read2 || !sem_write2) {
         perror("Ошибка открытия семафоров в child2");
         exit(EXIT_FAILURE);
@@ -34,23 +36,19 @@ int main() {
 
     while (1) {
         sem_wait(sem_read2);
-
         if (strcmp(shared_memory, "END") == 0) {
             break;
         }
-
         for (size_t i = 0; shared_memory[i]; i++) {
-            if (isspace((unsigned char)shared_memory[i])) {
+            if (isspace((unsigned char) shared_memory[i])) {
                 shared_memory[i] = '_';
             }
         }
-
         sem_post(sem_write2);
     }
 
     munmap(shared_memory, BUF_SIZE);
     sem_close(sem_read2);
     sem_close(sem_write2);
-
     return 0;
 }
--- lab3/src/parent.c
+++ lab3/src/parent.c
@@ -24,23 +24,20 @@ int main() {
         perror("Ошибка установки размера памяти");
         exit(EXIT_FAILURE);
     }
-
-    char *shared_memory = mmap(NULL, BUF_SIZE, PROT_READ | PROT_WRITE, MAP_SHARED, shm_fd, 0);
+    char *shared_memory = mmap(NULL, BUF_SIZE, PROT_READ | PROT_WRITE, MAP_SHARED,
+                               shm_fd, 0);
     if (shared_memory == MAP_FAILED) {
         perror("Ошибка маппинга памяти");
         exit(EXIT_FAILURE);
     }
-
     sem_t *sem_read1 = sem_open(SEM_READ1_NAME, O_CREAT, 0666, 0);
     sem_t *sem_write1 = sem_open(SEM_WRITE1_NAME, O_CREAT, 0666, 1);
     sem_t *sem_read2 = sem_open(SEM_READ2_NAME, O_CREAT, 0666, 0);
     sem_t *sem_write2 = sem_open(SEM_WRITE2_NAME, O_CREAT, 0666, 0);
-
     if (!sem_read1 || !sem_write1 || !sem_read2 || !sem_write2) {
         perror("Ошибка создания семафоров");
         exit(EXIT_FAILURE);
     }
-
     pid_t child1 = fork();
     if (child1 == -1) {
         perror("Ошибка создания child1");
@@ -51,7 +48,6 @@ int main() {
         perror("Ошибка запуска child1");
         exit(EXIT_FAILURE);
     }
-
     pid_t child2 = fork();
     if (child2 == -1) {
         perror("Ошибка создания child2");
@@ -62,55 +58,44 @@ int main() {
         perror("Ошибка запуска child2");
         exit(EXIT_FAILURE);
     }
-
     printf("Введите строки (Enter для завершения):\n");
     char buffer[BUF_SIZE];
     while (1) {
         if (!fgets(buffer, BUF_SIZE, stdin)) {
             break;
         }
-
         if (buffer[0] == '\n') {
             sem_wait(sem_write1);
             strcpy(shared_memory, "END");
             sem_post(sem_read1);
-
             sem_wait(sem_write1);
             sem_post(sem_read2);
-
             break;
         }
-
         sem_wait(sem_write1);
         strcpy(shared_memory, buffer);
         sem_post(sem_read1);
-
         sem_wait(sem_write1);
         sem_post(sem_read2);
-
         sem_wait(sem_write2);
         printf("> %s\n", shared_memory);
         sem_post(sem_write1);
     }
 
+
     sem_wait(sem_write1);
     strcpy(shared_memory, "END");
     sem_post(sem_read1);
-
     sem_wait(sem_write1);
     sem_post(sem_read2);
-
     waitpid(child1, NULL, 0);
     waitpid(child2, NULL, 0);
-
     munmap(shared_memory, BUF_SIZE);
     shm_unlink(SHM_NAME);
-
     sem_close(sem_read1);
     sem_close(sem_write1);
     sem_close(sem_read2);
     sem_close(sem_write2);
-
     sem_unlink(SEM_READ1_NAME);
     sem_unlink(SEM_WRITE1_NAME);
     sem_unlink(SEM_READ2_NAME);
